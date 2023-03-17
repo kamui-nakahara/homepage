e5a39ab7567e31e8a8721d74441df3cb9713cd6c
kamui-nakahara <83156405+kamui-nakahara@users.noreply.github.com>
2023-03-17 18:05:36 +0900

    aaa

---
+++ script.js
@@ -0,0 +1,4 @@
+header=document.getElementById("header");
+main=document.getElementById("main");
+footer=document.getElementById("footer");
+main.style.height=innerHeight-header.clientHeight-footer.clientHeight;
